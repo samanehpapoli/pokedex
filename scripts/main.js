f3376167a64364ecd878642430ebd977639d4a31
Samaneh Papoli <faalsamaneh@gmail.com>
2025-05-12 18:10:43 +0200

    Templates ausgelagert, Funktionen optimiert und Scroll-/Tab-Navigation verbessert

--- scripts/main.js
+++ scripts/main.js
@@ -1,65 +1,107 @@
+let allPokemons = [];
+let offset = 0;
+const LIMIT = 20;
 function init() {
   renedrePokemons();
 }
+
 async function getData(url) {
-   let responsData = await fetch(url);
-     return  await responsData.json();
-
+  let responsData = await fetch(url);
+  return await responsData.json();
 }
+
 async function renedrePokemons() {
-   showLoading();
-  // let pokemonsRespon = await fetch("https://pokeapi.co/api/v2/pokemon?limit=20&offset=0");
-  // let pokemons = await pokemonsRespon.json();
-let pokemons =await getData("https://pokeapi.co/api/v2/pokemon?limit=20&offset=0")
-
+  showLoading();
+  let pokemons = await getData(`https://pokeapi.co/api/v2/pokemon?limit=${LIMIT}&offset=${offset}`);
   let pokemonselement = document.getElementById("pokemons");
-  pokemonselement.innerHTML = "";
   for (const pokemon of pokemons.results) {
-    // let pokemonRespon = await fetch(pokemon.url);
-    // let pokemonDetail = await pokemonRespon.json();
-    let pokemonDetail = await getData(pokemon.url)
-
+    allPokemons.push(pokemon);
+    let pokemonDetail = await getData(pokemon.url);
     pokemonselement.innerHTML += getPokemonTemplate(pokemonDetail);
-    renderPokemonTypes(pokemonDetail );
+    renderPokemonTypes(pokemonDetail, "types-" + pokemonDetail.id);
   }
   hideloading();
 }
 
 function showLoading() {
-  document.getElementById('loading').classList.remove('d-none');
-  document.querySelector('body').classList.add('o-hidden');
+  document.getElementById("loading").classList.remove("d-none");
+  hideBodyScroll();
 }
 
 function hideloading() {
-  document.getElementById('loading').classList.add('d-none');
-   document.querySelector('body').classList.remove('o-hidden');
-  
+  document.getElementById("loading").classList.add("d-none");
+  showBodyScroll();
 }
-function renderPokemonTypes(pokemonDetail) {
-   let pokemonTypeElement = document.getElementById("types-" + pokemonDetail.id);
-    pokemonTypeElement.innerHTML ='';
-    pokemonDetail.types.forEach(singleType => {
-       pokemonTypeElement.innerHTML += getPokemonTypesTemplate(singleType);
-    });
-  
-}
-function openDetail() {
-  document.getElementById('overlay').classList.remove('d-none');
-}
-function closeDetail() {
-  document.getElementById('overlay').classList.add('d-none');
-  
-}
-function changeTab(clickesElement, tab){
 
-let tabsSelector = document.querySelectorAll('.tabs > div');
-tabsSelector.forEach(tabSelector => {
-tabSelector.classList.remove('active');
-});
-clickesElement.classList.add('active');
- let tabsDetailSelector = document.querySelectorAll('.tabsDetail > div');
- tabsDetailSelector.forEach(tabDetailSelector => {
-  tabDetailSelector.classList.add('d-none');
- });
- document.getElementById(tab).classList.remove('d-none');
+function renderPokemonTypes(pokemonDetail, elementId) {
+  let pokemonTypeElement = document.getElementById(elementId);
+  pokemonTypeElement.innerHTML = "";
+  pokemonDetail.types.forEach((singleType) => {
+    pokemonTypeElement.innerHTML += getPokemonTypesTemplate(singleType);
+  });
+}
+
+async function openDetail(pokemonId) {
+  let pokemonDetail = await getData("https://pokeapi.co/api/v2/pokemon/" + pokemonId);
+  document.getElementById("overlay").innerHTML = getPokemonDetailTemplate(pokemonDetail);
+  renderPokemonTypes(pokemonDetail, "overlay-types-" + pokemonDetail.id);
+  renderPokemonStats(pokemonDetail);
+  document.getElementById("overlay").classList.remove("d-none");
+  hideBodyScroll();
+}
+
+function renderPokemonStats(pokemonDetail) {
+  let statusElement = document.getElementById("status");
+  statusElement.innerHTML = "";
+  pokemonDetail.stats.forEach((stat) => {
+    statusElement.innerHTML += getPokemonStatTemplate(stat);
+  });
+}
+
+function closeDetail(clickesElement, event) {
+  if (event.target == clickesElement) {
+    document.getElementById("overlay").classList.add("d-none");
+    showBodyScroll();
+  }
+}
+
+function nextPokemon(pokemonId) {
+  let nextPokemonId = pokemonId + 1;
+  if (pokemonId === allPokemons.length) {
+    nextPokemonId = 1;
+  }
+  openDetail(nextPokemonId);
+}
+
+function previousPokemon(pokemonId) {
+  let previousPokemonId = pokemonId - 1;
+  if (pokemonId === 1) {
+    previousPokemonId = allPokemons.length;
+  }
+  openDetail(previousPokemonId);
+}
+
+function changeTab(clickesElement, tab) {
+  let tabsSelector = document.querySelectorAll(".tabs > div");
+  tabsSelector.forEach((tabSelector) => {
+    tabSelector.classList.remove("active");
+  });
+  clickesElement.classList.add("active");
+  let tabsDetailSelector = document.querySelectorAll(".tabsDetail > div");
+  tabsDetailSelector.forEach((tabDetailSelector) => {
+    tabDetailSelector.classList.add("d-none");
+  });
+  document.getElementById(tab).classList.remove("d-none");
+}
+
+function showBodyScroll() {
+  document.querySelector("body").classList.remove("o-hidden");
+}
+
+function hideBodyScroll() {
+  document.querySelector("body").classList.add("o-hidden");
+}
+function lodeMorePokemon() {
+  offset += LIMIT;
+  renedrePokemons();
 }
--- scripts/template.js
+++ scripts/template.js
@@ -1,5 +1,5 @@
-function getPokemonTemplate(pokemonDetail){
-    return ` <div class="pokemon" onclick="openDetail()">
+function getPokemonTemplate(pokemonDetail) {
+  return ` <div class="pokemon" onclick="openDetail(${pokemonDetail.id})">
             <div class="big-circle bg-${pokemonDetail.types[0].type.name}"></div>
             <div class="hp">
                 <span>HP</span>
@@ -8,10 +8,7 @@ function getPokemonTemplate(pokemonDetail){
             <div class="main-info">
                 <img src="${pokemonDetail["sprites"]["other"]["official-artwork"]["front_default"]}" alt="" />
                 <h2 class="fc-${pokemonDetail.types[0].type.name}">${pokemonDetail.name}</h2>
-                <div class="types" id="types-${pokemonDetail.id}">
-                <span>Fire</span>
-                <span>Grass</span>
-                </div>
+                <div class="types" id="types-${pokemonDetail.id}"></div>
             </div>
             <div class="powers">
                 <div>
@@ -30,6 +27,49 @@ function getPokemonTemplate(pokemonDetail){
             </div>`;
 }
 function getPokemonTypesTemplate(singleType) {
-    return  `<span class="bg-${singleType.type.name}">${singleType.type.name}</span>`;
-    
+  return `<span class="bg-${singleType.type.name}">${singleType.type.name}</span>`;
+}
+function getPokemonDetailTemplate(pokemonDetail) {
+  return `<div class="pokemon-overlay bg-lg-${pokemonDetail.types[0].type.name}">
+          <img src="./assets/img/loso2.svg" class="logo" alt="logo" />
+          <div class="icons">
+            <img src="./assets/icon/left-arrow (1).png" alt="" onclick = "previousPokemon(${pokemonDetail.id})" />
+            <img src="./assets/icon/close.png" alt="" onclick="closeDetail(this,event)" />
+            <img src="./assets/icon/right-arrow (1).png" alt="${pokemonDetail.name}" onclick="nextPokemon(${pokemonDetail.id})" />
+          </div>
+          <div class="top">
+            <img src="${pokemonDetail["sprites"]["other"]["official-artwork"]["front_default"]}" alt="${pokemonDetail.name}" />
+            <div class="code">#${pokemonDetail.id}</div>
+            <h2 class="name">${pokemonDetail.name}</h2>
+            <div class="abilities" id="overlay-types-${pokemonDetail.id}"></div>
+          </div>
+          <div class="tabs">
+            <div class="active" onclick="changeTab(this, 'detail')">Details</div>
+            <div onclick="changeTab(this, 'status')">Status</div>
+          </div>
+          <div class="tabsDetail">
+            <div class="detail" id="detail">
+            <div>
+              <div>
+                <span>Height</span>
+                <span>${pokemonDetail.height}m</span>
+              </div>
+              <div>
+                <span>Weight</span>
+                <span>${pokemonDetail.weight}k</span>
+              </div>
+            </div>
+          </div>
+          <div class="status d-none" id="status"></div>
+          </div>
+        </div>`;
+}
+
+function getPokemonStatTemplate(stat) {
+    let baseStat =Math.max(1 ,Math.min (stat.base_stat, 100))
+  return `<div>
+            <div class="title">${stat.stat.name}</div>
+            <div class="progressbar">
+            <span style="width: ${baseStat}%"></span>
+        </div>`;
 }
